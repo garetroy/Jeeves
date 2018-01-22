b3cc29fe94367e7c743b6af4e3ca2d9f908cac36
Garett Roberts <mygarett@gmail.com>
2018-01-22 09:27:26 -0800

    Added new jeevesuser abstraction

---
+++ jeeves/jeevesuser.py
@@ -0,0 +1,33 @@
+from discord import User as Us
+class JeevesUser:
+    def __init__(self,userinstance):
+        if not isinstance(userinstance,Us):
+            raise ValueError
+    
+        self.id         = userinstance.id
+        self.name       = userinstance.name
+        self.points     = 0 
+        self.leaugename = None
+        self.leaugeid   = None 
+        self.runescapen = None #runescapename
+        self.remindlive = (None,None) #(leaugeid,True/False)
+        self.remindlupd = False #Message if new leauge patch
+        self.callme     = (None,None) #(nickname, True/False)  
+
+    def __eq__(self,other):
+        if not isinstance(other,JeevesUser):
+            raise ValueError
+        
+        return other.id == self.id
+
+    def __ne__(self,other):
+        return not self == other
+
+    def __gt__(self,other):
+        if not isinstance(other,JeevesUser):
+            raise ValueError
+        
+        return self.points > other.points
+
+    def __lt__(self,other):
+        return not self > other
