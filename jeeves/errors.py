96ee3f56b2cb079b99b377c85be4393b441ba12c
Garett Roberts <mygarett@gmail.com>
2018-01-24 21:03:06 -0800

    Added comments to errors

--- jeeves/errors.py
+++ jeeves/errors.py
@@ -1,26 +1,75 @@
 class JeevesException(Exception):
     """ 
-        Base exception class for jeeves
+        Base exception class for jeeves.
     """
     pass
 
 
 class UserNotAdded(JeevesException):
-    def __init__(self, name, *args): 
+    """
+        An JeevesException that raises when there is not a user added to the
+        server or "not found".
+
+        Attributes
+        ----------
+        name : string
+            The name of the Member not found.
+        
+        message : string
+            The message that is generated from this error.
+    """
+    def __init__(self, name): 
         self.name    = name
         self.message = "{} is not added to anything or is a non-existant user"\
             .format(name)
 
 class UserInsufficentPermissions(JeevesException):
-    def __init__(self, name, *args):
+    """
+        An JeevesException that raises when a Member does not have sufficent
+        permissions.
+
+        Attributes
+        ----------
+        name : string
+            The name of the Member not found.
+        
+        message : string
+            The message that is generated from this error.
+    """
+    def __init__(self, name):
         self.name    = name
         self.message = "{} does not have sufficent permissions".format(name)
 
 class BadInput(JeevesException):
+    """
+        An JeevesException that raises when the input was bad.
+
+        Attributes
+        ----------
+        message : string
+            The message that is generated from this error.
+    """
     def __init__(self, message):
         self.message = message
 
-class InvalidType(JeevesException):
+class InvalidType(JeevesException
+    """
+        An JeevesException that raises when the input was bad.
+
+        Attributes
+        ----------
+        type1 : type
+            The type we got.
+
+        type2 : type
+            The type we expected.
+    
+        location : string
+            A string with the location of the incident.
+        
+        message : string
+            The message that is generated from this error.
+    """):
     def __init__(self, type1, type2, location):
         self.message = "Got type:{} but expected type:{} - in {}".format(\
             type1,type2,location)
